9626d4f81803185fa6e8a70ba14df53c8b1e5cac
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-08-13 11:32:43 +0200

    fine shyt 09

--- components/fixed-whatsapp-icon.tsx
+++ components/fixed-whatsapp-icon.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 export const FixedWhatsappIcon = () => {
   return (
-    <a className="flex z-50 items-center gap-2 p-2 rounded-full bg-white/70 backdrop-blur-md fixed md:bottom-10 md:right-10 bottom-4 right-4" href="https://wa.me/+96555501387" target="_blank" rel="noopener noreferrer">
+    <a className="group flex z-50 items-center gap-2 p-2 rounded-full bg-white/50 backdrop-blur-md fixed md:bottom-10 md:right-10 bottom-4 right-4" href="https://wa.me/+96555501387" target="_blank" rel="noopener noreferrer">
     <Image
       src="/whatsapp.webp"
       alt="WhatsApp Icon"
-      width={20}
-      height={20}
-      quality={50}
+      width={300}
+      height={300}
+      // quality={100}
       priority = {false}
-      className="h-8 w-8"
+      className="h-10 w-10 group-hover:scale-110 transition-transform duration-200"
       />
     {/* <p className="text-sm">+965 555 01387</p> */}
   </a>
